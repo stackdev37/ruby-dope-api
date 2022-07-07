4895c4ffb6d5a7906ec21bf92bc45920cbc12600
stackdev37 <stackdev37@gmail.com>
2022-07-07 01:33:22 -0700

    restful api Impl with tests (seeds - dummy)

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,44 @@
 class Api::V1::ProductsController < ApplicationController
+  # index renders all times in the table
   def index
+    products = Product.all
+    render json: products, status: 200
   end
 
-  def show
+  # if the product saves successfully, we render json data for the product
+  # if the product does not save correctly, we render json error code
+  def create
+    product = Product.new(
+      name: prod_params[:name], 
+      brand: prod_params[:brand], 
+      price: prod_params[:price], 
+      description: prod_params[:description]
+    )
+    if product.save
+      render json: product, status: 200
+    else
+      render json: {error: 'error creating review.'}
+    end
   end
+
+  # This method looks up the product by id, it we find out. 
+  # Otherwise, we render error message
+  def show
+    product = Product.find_by(id: params[:id])
+    if product
+      render json: product, status: 200
+    else
+      render json: {error: 'product not found'}
+    end
+  end
+
+  private
+    def prod_params
+      params.require(:product).permit([
+        :name, 
+        :brand, 
+        :price, 
+        :description
+      ])
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+    {
+        name: "test1", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 1"
+    }, 
+    {
+        name: "test2", 
+        brand: "brand2", 
+        price: "300.00USD", 
+        description: "description 2"
+    }, 
+    {
+        name: "test3", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 3"
+    }, 
+    {
+        name: "test4", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 1"
+    }, 
+    {
+        name: "test5", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 1"
+    }, 
+    {
+        name: "test6", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 1"
+    }, 
+    {
+        name: "test7", 
+        brand: "brand1", 
+        price: "300.00USD", 
+        description: "description 1"
+    }
+])
